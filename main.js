18136964357846c42d3e7afaa8a54455943800b0
ishaongithub <75757332+ishaongithub@users.noreply.github.com>
2021-10-05 21:26:39 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,25 @@ function setup() {
   canvas.center();
   video = createCapture(VIDEO);
   video.hide();
+  classifier=ml5.imageClassifier('MobileNet', modelLoaded);
 }
 
+function draw(){
+  image(video,0,0,250,250);  
+  classifier.classify(video, gotResult);
+}
+function modelLoaded(){
+  console.log("Model Loaded!");
+}
+function gotResult(error, results){
+  if (error){
+    console.error(error);
 
+  }
+  else{
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML=results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
 
+  }
+}
